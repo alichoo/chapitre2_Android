7800a7715b877adb751197381215b11dd65bf032
alichooo <ali.chouikhi2@gmail.com>
2025-11-21 22:24:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chapitre2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.chapitre2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chapitre2.databinding.ActivityMainBinding;
+import com.example.chapitre2.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+TextView textView;
+EditText greeting;
+Button button;
+
+ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        EdgeToEdge.enable(this);
+      ///  setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+       /// textView = findViewById(R.id.greeting_text);
+        textView = binding.greetingText;
+      ///  greeting = findViewById(R.id.name_input);
+        greeting = binding.nameInput;
+      ///  button = findViewById(R.id.greet_button);
+      ///  button = findViewById(R.id.greet_button);
+        button = binding.greetButton;
+
+
+    }
+
+    public void greet(View view) {
+       /// Intent intent = new Intent(this, LoginActivity.class);
+        /// startActivity(intent);
+       textView.setText( textView.getText()+"\nHello Mr/Mrs: "+greeting.getText());
+       Log.d("MainActivity","Hello Mr/Mrs: "+greeting.getText());
+       Toast.makeText(this,"Hello Mr/Mrs: "+greeting.getText(),Toast.LENGTH_SHORT).show();
+    }
+}
